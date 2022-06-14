556aa05978d185a71657a1b1bb3afdb0a7c78f80
vinicius.pimenta <vinicius.pimenta@dataprev.gov.br>
2022-06-14 17:17:18 -0300

    task -  devoluçao filtro a finalizar

--- src/app/locacao/devolucao/devolucao.component.ts
+++ src/app/locacao/devolucao/devolucao.component.ts
@@ -1,9 +1,13 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
+import { Clientes } from 'src/app/clientes/model/clientes';
+import { ClientesService } from 'src/app/clientes/services/clientes.service';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 import { Locacao } from '../model/locacao';
@@ -17,15 +21,21 @@ import { LocacaoService } from '../services/locacao.service';
 export class DevolucaoComponent implements OnInit {
 
   locacao$: Observable<Locacao[]>;
-  displayedColumns = ['cliente', 'jogo', 'funcionario','dataLocacao', 'dataDevolucao', 'actions'];
+  clientes$: Observable<Clientes[]>;
+  displayedColumns = ['cliente', 'jogo', 'funcionario', 'dataLocacao', 'dataDevolucao', 'actions'];
+  form: FormGroup;
+  selectedValue: any;
+  locacao: any;
 
   constructor(
     private service: LocacaoService,
+    private clientesService: ClientesService,
     public dialog: MatDialog,
     private router: Router,
     private route: ActivatedRoute,
     private snackBar: MatSnackBar,
-    private location: Location
+    private location: Location,
+    private formBuilder: FormBuilder
   ) {
     this.locacao$ = this.service.list()
       .pipe(
@@ -34,6 +44,18 @@ export class DevolucaoComponent implements OnInit {
           return of([])
         })
       );
+
+    this.clientes$ = this.clientesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar clientes');
+          return of([])
+        })
+      );
+
+    this.form = this.formBuilder.group({
+      cliente: [null],
+    });
   }
 
   ngOnInit(): void {
@@ -49,7 +71,15 @@ export class DevolucaoComponent implements OnInit {
     this.location.back();
   }
 
-  onEdit(id: string){
+  onEdit(id: string) {
     this.router.navigate(['locacao/devolver', id]);
   }
+
+  getClientePorNome() {
+    console.log(this.form.value);
+    console.log(this.locacao);
+    const locacao = this.locacao$;
+    return locacao;
+  }
+
 }
